528514a7265359961fd0d74efdd9e85070b040f9
Leandro Parrado <lparrdev@gmail.com>
2024-07-10 20:44:51 -0400

    TS1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int dividedNumber = toBeDivided / 5;
+        System.out.println(dividedNumber);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider divider = new NumberDivider();
+        divider.divide(10);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+
     public void convert(float minutes) {
+        float seconds = (minutes * 60);
+        System.out.println(seconds);
     }
+
+    public static void main(String[] args) {
+        TimeConvertor converter = new TimeConvertor();
+        converter.convert(90);
+    }
+
+
 }
