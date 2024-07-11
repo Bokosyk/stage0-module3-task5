f01c3a57b3d4cffb66a90b17a63016522b6dfd97
Leandro Parrado <lparrdev@gmail.com>
2024-07-10 20:52:39 -0400

    TS2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,12 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int dividedNumber = toBeDivided / 5;
+        // (float) ensures that the division is performed in floating-point arithmetic, giving the correct result of 3.8.
+        float dividedNumber = (float) toBeDivided / 5;
         System.out.println(dividedNumber);
     }
 
     public static void main(String[] args) {
         NumberDivider divider = new NumberDivider();
-        divider.divide(10);
+        divider.divide(19);
     }
 }
